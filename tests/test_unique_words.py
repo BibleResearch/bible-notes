46c14bc402d1b75f653b5fe54ef03f87870c0540
Floyd Hightower <floyd.hightower27@gmail.com>
2018-02-15 07:06:28 -0400

    Adding tests to find typos and making minor formatting changes

---
+++ tests/test_unique_words.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Find duplicate words."""
+
+import os
+
+
+def find_duplicate_words(file_path):
+    errors = 0
+
+    with open(file_path) as f:
+        file_text = f.read()
+
+    # TODO: add validation that we are only finding words and not markdown characters using a regex
+    words = [word for word in file_text.split(' ') if word != '' and word != '>']
+
+    for index in range(0, len(words)):
+        this_word = words[index]
+        try:
+            next_word = words[index + 1]
+        except IndexError:
+            break
+
+        try:
+            assert this_word != next_word
+        except AssertionError as e:
+            print("Found duplicate words: '{}' in {}".format(this_word, file_path))
+            errors += 1
+
+    return errors
+
+
+def test_duplicate_words_ot():
+    errors = 0
+    for path, dirs, files in os.walk(os.path.abspath(os.path.join(os.path.dirname(__file__), '../old_testament'))):
+        for file_name in files:
+            file_path = os.path.join(path, file_name)
+            errors += find_duplicate_words(file_path)
+    assert errors == 0
+
+
+def test_duplicate_words_nt():
+    errors = 0
+    for path, dirs, files in os.walk(os.path.abspath(os.path.join(os.path.dirname(__file__), '../new_testament'))):
+        for file_name in files:
+            file_path = os.path.join(path, file_name)
+            errors += find_duplicate_words(file_path)
+    assert errors == 0
