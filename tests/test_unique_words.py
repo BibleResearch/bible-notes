8d91ef547a1084d8c519649c7dd70ce161e823d7
Floyd Hightower <floyd.hightower27@gmail.com>
2018-04-03 06:00:09 -0400

    Minor updates to esther and updating the testing script

--- tests/test_unique_words.py
+++ tests/test_unique_words.py
@@ -11,11 +11,11 @@ def find_duplicate_words(file_path, file_name):
     errors = 0
 
     with open(file_path) as f:
-        file_text = f.read().replace('.', ' ')
+        file_text = f.read()
 
-    word_pattern = '[^*]\w+'
+    word_pattern = '[^<]\w+'
 
-    words = [word.strip() for word in re.findall(word_pattern, file_text)]
+    words = [word.strip().lower() for word in re.findall(word_pattern, file_text)]
 
     for index in range(0, len(words)):
         this_word = words[index]
@@ -25,7 +25,7 @@ def find_duplicate_words(file_path, file_name):
             continue
 
         # ignore words that are the name of the book of the bible
-        if this_word.lower() in file_name:
+        if this_word in file_name:
             continue
 
         try:
@@ -46,8 +46,9 @@ def test_duplicate_words_ot():
     errors = 0
     for path, dirs, files in os.walk(os.path.abspath(os.path.join(os.path.dirname(__file__), '../old_testament'))):
         for file_name in files:
-            file_path = os.path.join(path, file_name)
-            errors += find_duplicate_words(file_path, file_name)
+            if file_name.endswith('.md'):
+                file_path = os.path.join(path, file_name)
+                errors += find_duplicate_words(file_path, file_name)
 
     print("\nFound {} errors in the OT".format(errors))
     assert errors == 0
@@ -57,8 +58,9 @@ def test_duplicate_words_nt():
     errors = 0
     for path, dirs, files in os.walk(os.path.abspath(os.path.join(os.path.dirname(__file__), '../new_testament'))):
         for file_name in files:
-            file_path = os.path.join(path, file_name)
-            errors += find_duplicate_words(file_path, file_name)
+            if file_name.endswith('.md'):
+                file_path = os.path.join(path, file_name)
+                errors += find_duplicate_words(file_path, file_name)
 
     print("\nFound {} errors in the NT".format(errors))
     assert errors == 0
